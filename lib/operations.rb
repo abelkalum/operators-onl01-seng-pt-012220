bb973ca02b268241d4be5ee8a2e8486cbb83aa21
Abel Kalum <chumbabel@gmail.com>
2020-01-22 23:52:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
   if speed > 60 ? true : false
-    returneturn true
+    return true
   if speed < 40 ? true : false
-    returneturn true
+    return true
   else
     return false
   end
